e8e4550b0d0136552cc18689fb6b40759b2a8638
EgorBYV <egorvrublevskij@gmail.com>
2021-12-29 21:34:54 +0300

    My first game

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+from settings import Settings 
+
+class AlienInvasion():
+	"""Класс для управления ресурсами и поведением игры"""
+	
+	def __init__(self):
+		"""Инициализирует игру и создает игровые ресурсы"""
+		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_wight, self.settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+		# Назначение цвета фона.
+		self.bg_color = (self.settings.bg_color)
+
+	def run_game(self):
+		"""Запуск основного цикла игры."""
+		while True:
+			# Отслеживание событий клавиатуры и мыши.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+			  		sys.exit()
+
+			# При каждом проходе цикла перересовывается экран.
+			self.screen.fill(self.bg_color)
+
+			# Отображение последнего прорисованного экрана
+			pygame.display.flip()
+
+if __name__ == '__main__':
+	# Создание экзeмпляра и запуск игры.
+	ai = AlienInvasion()
+	ai.run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+	"""Класс для хранения всех настроек игры Alien Invasion."""
+
+	def __init__(self):
+		"""Инициализирует настройки игры."""
+	
+		# Параметры экрана
+		self.screen_wight = 1200
+		self.screen_height = 800
+		self.bg_color = (0,100,230)
+		
